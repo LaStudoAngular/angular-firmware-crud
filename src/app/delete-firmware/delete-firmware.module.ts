1a91d788707c80ab39a6541bb0d552f1b721b302
Moe Green <gearmobile@gmail.com>
2019-08-28 20:58:29 +0300

    feat: fill delete-firmware component

--- src/app/delete-firmware/delete-firmware.module.ts
+++ src/app/delete-firmware/delete-firmware.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
+// MODULES
+import { SharedModule } from '../@shared/shared.module';
 import { DeleteFirmwareRoutingModule } from './delete-firmware-routing.module';
+
+// COMPONENTS
 import { DeleteFirmwareComponent } from './delete-firmware/delete-firmware.component';
 
 @NgModule({
   declarations: [DeleteFirmwareComponent],
-  imports: [CommonModule, DeleteFirmwareRoutingModule],
+  imports: [DeleteFirmwareRoutingModule, SharedModule],
 })
 export class DeleteFirmwareModule {}
--- src/app/delete-firmware/delete-firmware/delete-firmware.component.ts
+++ src/app/delete-firmware/delete-firmware/delete-firmware.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FirmwareService } from '../../@services/firmware.service';
+import { Firmware } from '../../@models/firmware';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'cr-delete-firmware',
@@ -6,7 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delete-firmware.component.scss'],
 })
 export class DeleteFirmwareComponent implements OnInit {
-  constructor() {}
+  form: FormGroup;
+  firmware: Firmware;
+  errorMessage = '';
 
-  ngOnInit() {}
+  constructor(private fb: FormBuilder, private firmwareService: FirmwareService) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      boot_version: [null, [Validators.required]],
+      fw_version: [null, [Validators.required]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      const bootVersion = this.form.get('boot_version').value;
+      const fwVersion = this.form.get('fw_version').value;
+      this.firmwareService.deleteFirmware(bootVersion, fwVersion).subscribe(
+        () => {},
+        error => {
+          this.errorMessage = error.status
+            ? environment.FirmwareDictionary.DELETE[error.status]
+            : null;
+        },
+      );
+    }
+  }
 }
